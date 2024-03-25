be75048e93845af9eb3963c6ccf313bdd8d06949
emilianogastaldo <emiliano0303@gmail.com>
2024-03-25 15:05:27 +0100

    aggiunta type_id a ProjectFactory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Type;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -18,15 +20,21 @@ class ProjectFactory extends Factory
      */
     public function definition(): array
     {
+        // creo la cartella per le immagini nello storage
         Storage::makeDirectory('project_images');
+        // creo titolo e slug
         $title = fake()->text(20);
         $slug = Str::slug($title);
+        // creo immagini con nome personalizzato e le salvo nello storage
         $image = fake()->image(null, 300, 300);
         $img_url = Storage::putFileAs('project_images', $image, "$slug.png");
+        // creo l'array degli id dei tipi
+        $type_ids = Type::pluck('id')->toArray();
         return [
             'title' => $title,
             'slug' => $slug,
             'content' => fake()->paragraphs(15, true),
+            'type_id' => Arr::random($type_ids),
             'image' => $img_url,
             'is_published' => fake()->boolean(),
         ];
