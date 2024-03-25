4538911dd09d0c73a79b9947f037ea5d12e252a2
emilianogastaldo <emiliano0303@gmail.com>
2024-03-25 15:16:24 +0100

    aggiunta la tipologia nello show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,21 +3,27 @@
 @section('title', 'Projects')
 
 @section('content')
-    <header class="my-5">
+    <header class="mt-3">
         <h1>{{$project->title}}</h1>
-    </header>
-    <div class="clearfix">
-        @if ($project->image)
-        <img src="{{$project->printImage()}}" alt="{{$project->post}}" class="me-4 float-start img-fluid">
+        @if ($project->type->label)
+        <p>Tipologia: {{$project->type->label}}</p>            
         @endif
-        <p>{{$project->content}}</p>
-        <div>
-            <p><strong>Creato il: </strong>{{$project->getFormatedDate('created_at', 'd-m-Y H:i:s')}}</p>
-            <p><strong>Ultima modifica il: </strong>{{$project->getFormatedDate('updated_at', 'd-m-Y H:i:s')}}</p>
+    </header>
+    <hr>
+    <section class="py-3">
+        <div class="clearfix">
+            @if ($project->image)
+            <img src="{{$project->printImage()}}" alt="{{$project->post}}" class="me-4 float-start img-fluid">
+            @endif
+            <p>{{$project->content}}</p>
+            <div>
+                <p><strong>Creato il: </strong>{{$project->getFormatedDate('created_at', 'd-m-Y H:i:s')}}</p>
+                <p><strong>Ultima modifica il: </strong>{{$project->getFormatedDate('updated_at', 'd-m-Y H:i:s')}}</p>
+            </div>
         </div>
-    </div>
-    <footer class="mt-5 d-flex justify-content-between alig-items-center">
-        <a href="{{route('admin.projects.index')}}" class="btn btn-outline-secondary"><i class="far fa-hand-point-left me-2"></i>Torna indietro</a>
+    </section>
+        <footer class="d-flex justify-content-between alig-items-center">
+            <a href="{{route('admin.projects.index')}}" class="btn btn-outline-secondary"><i class="far fa-hand-point-left me-2"></i>Torna indietro</a>
         <div class="d-flex justify-content-between alig-items-center gap-2">
             <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-outline-warning"><i class="fas fa-pen me-2"></i>Modifica</a>
             <form action="{{route('admin.projects.destroy', $project)}}" method="POST" class="delete-form" data-bs-toggle="modal" data-bs-target="#modal">
