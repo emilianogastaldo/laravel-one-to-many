7cbcc20ae7dfbe9b4d34456b6127a9ce5bf36264
emilianogastaldo <emiliano0303@gmail.com>
2024-03-25 14:55:44 +0100

    creato controller e seed

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Faker\Generator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+        $types = ['FrontEnd', 'BackEnd', 'FullStack', 'Design', 'UI/UX', 'DataBase', null];
+        foreach ($types as $label) {
+            $type = new Type();
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
+    }
+}
